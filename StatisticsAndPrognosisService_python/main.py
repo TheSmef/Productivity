2e358eed34cc8ab8421aaed6e30cf8537757cc80
Alexander Sokolov <sokolov19868@gmail.com>
2023-12-29 05:53:35 +0300

    Удалена лишняя директория проекта статистики

--- StatisticsAndPrognosisService_python/main.py
+++
@@ -1,8 +0,0 @@
-from Calculations.model import Model
-from RabbitReader.rabbit_reader import RabbitReader
-
-
-if __name__ == "__main__":
-    md = Model()
-    Rabbit_response = RabbitReader().receive_message()
-
